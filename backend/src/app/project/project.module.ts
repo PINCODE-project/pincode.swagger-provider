f6da9dacd0608de84fc21f13c7a6b2208e1ca640
CatDevelop <maks.rozhkov.2004@gmail.com>
2024-07-11 02:10:24 +0500

    feat: new swagger theme

--- backend/src/app/project/project.module.ts
+++ backend/src/app/project/project.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
-import { ProjectService } from './project.service';
-import { ProjectController } from './project.controller';
+import {Module} from '@nestjs/common';
+import {ProjectService} from './project.service';
+import {ProjectController} from './project.controller';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Project} from "./entities/project.entity";
 import {Microservice} from "../microservice/entities/microservice.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project, Microservice])],
-  controllers: [ProjectController],
-  providers: [ProjectService],
+    imports: [TypeOrmModule.forFeature([Project, Microservice])],
+    controllers: [ProjectController],
+    providers: [ProjectService],
 })
-export class ProjectModule {}
+export class ProjectModule {
+}
